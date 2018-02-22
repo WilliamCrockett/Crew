bc06dfd97b0a6c1d31f4d9a62bacea2ca5ee3261
WilliamCrockett <william@sailing.co.za>
2018-02-21 22:11:29 -0500

    Attempted to add row click handlers
    Not working yet. Will branch and work on it

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -2,6 +2,14 @@
 
 const ui = require('./ui.js')
 
+let targetID = ''
+
+const clickedID = function (event) {
+  alert(event.target.id)
+  targetID = (event.target.id)
+  alert(targetID)
+}
+
 const onSignIn = function (/* event */) {
   event.preventDefault()
   ui.onSignInSuccess()
@@ -15,14 +23,21 @@ const toSignIn = function () {
   ui.onToSignIn()
 }
 
+const onRowClick = function () {
+  alert('look at me now!')
+}
+
 const addHandlers = () => {
+  $('#contentTable').click(clickedID)
   $('#signIn').on('submit', onSignIn)
   $('#signUpLink').on('click', toSignUp)
   $('#signInLink').on('click', toSignIn)
+  $('#td1').on('click', onRowClick)
 }
 
 module.exports = {
   addHandlers,
   toSignUp,
-  toSignIn
+  toSignIn,
+  onRowClick
 }
